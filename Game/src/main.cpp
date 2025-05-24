23485bcb346b79f78819901b604dd6cb7cdf12fa
danie <danielgarciacarrete@gmail.com>
2025-05-24 19:27:25 +0200

    Fixed a few bugs with rendering and the apples

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Engine.hpp>
 #include <GLFW/glfw3.h>
+#include <imgui.h>
 #include <random>
 
 constexpr int WIDTH = 32, HEIGHT = 18;
 
-constexpr glm::vec4 SNAKE_COLOR = {0.09, 0.671, 0, 1};
-constexpr glm::vec4 APPLE_COLOR = {0.98, 0.216, 0, 1};
+constexpr glm::vec4 SNAKE_COLOR = {0.09, 0.471, 0, 1};
+constexpr glm::vec4 APPLE_COLOR = {0.98, 0.116, 0, 1};
 
 class SnakeGame: public fly::Scene {
 public:
@@ -19,9 +20,21 @@ public:
             std::filesystem::path("assets/font.json")
         );
 
+        distX = std::uniform_int_distribution(0, WIDTH-1);
+        distY = std::uniform_int_distribution(0, HEIGHT-1);
+
+        start();
+    }
+
+    void start() {
+        dead = false;
+        newDir = dir = 0;
+        this->threshold = 0.4;
+
+        snake.clear();
         snake.push_back({WIDTH/2, HEIGHT/2});
         snake.push_back({WIDTH/2, HEIGHT/2 + 1});
-
+        
         rollApple();
     }
 
@@ -31,34 +44,33 @@ public:
 
         if(dead) {
             //std::cout << "DEAD" << std::endl;
-            engine.getTextRenderer().renderText("DS_DIGITAL", "GAME OVER", {1280/2, 720/2 - 200}, fly::Align::CENTER, 200, {1, 1, 1, 1});
+            engine.getTextRenderer().renderText("DS_DIGITAL", "GAME OVER", {1280/2, 720/2 - 150}, fly::Align::CENTER, 200, {1, 1, 1, 1});
             
-            if(engine.getWindow().keyJustPressed(GLFW_KEY_ENTER))
-                dead = false;
+            if(engine.getWindow().keyJustPressed(GLFW_KEY_ENTER)) {
+                start();
+            }
             
             return;
         }
         
         this->ts += dt;
         
-        bool touched = false;
         if(engine.getWindow().keyJustPressed(GLFW_KEY_LEFT) && dir != 1) {
-            dir = 3;
-            touched = true;
+            newDir = 3;
         } else if(engine.getWindow().keyJustPressed(GLFW_KEY_RIGHT) && dir != 3) {
-            dir = 1;
-            touched = true;
+            newDir = 1;
         }
 
         if(engine.getWindow().keyJustPressed(GLFW_KEY_UP) && dir != 2) {
-            dir = 0;
-            touched = true;
+            newDir = 0;
         } else if(engine.getWindow().keyJustPressed(GLFW_KEY_DOWN) && dir != 0) {
-            dir = 2;
-            touched = true;
+            newDir = 2;
         }
 
-        if(this->ts >= this->threshold || touched) {
+        float completion = this->ts / this->threshold;
+
+        if(completion >= 1) {
+            dir = newDir;
             this->ts = 0;
 
             glm::ivec2 newHead = snake.front() + glm::ivec2(incX[dir], incY[dir]);
@@ -68,7 +80,7 @@ public:
 
                 if(newHead == apple) {
                     rollApple();
-                    this->threshold -= 0.05;
+                    this->threshold -= 0.01;
                 } else {
                     snake.pop_back();
                 }
@@ -85,40 +97,41 @@ public:
 
         engine.getRenderer2d().renderQuad({tileWidth * apple.x, tileHeight * apple.y}, {tileWidth, tileHeight}, APPLE_COLOR);
 
-        /*for(int i=0; i<WIDTH; ++i) {
+        for(int i=0; i<WIDTH; ++i) {
             for(int j=0; j<HEIGHT; ++j) {
-                if(std::find(snake.begin(), snake.end(), glm::ivec2{i, j}) != snake.end() || std::find(snake.begin(), snake.end(), apple) != snake.end())
+                if(std::find(snake.begin(), snake.end(), glm::ivec2{i, j}) != snake.end() || glm::ivec2{i,j} == apple)
                     continue;
 
-                auto color = (i+j)%2? glm::vec4{0.588, 0.761, 0.529, 1} : glm::vec4{0.427, 0.741, 0.322, 1};
+                auto color = (i+j)%2? glm::vec4{0.488, 0.661, 0.429, 1} : glm::vec4{0.327, 0.641, 0.222, 1};
                 engine.getRenderer2d().renderQuad({tileWidth * i, tileHeight * j}, {tileWidth, tileHeight}, color);
             }
-        }*/
+        }
+
+        ImGui::LabelText("Pos:", "%d,%d", snake.front().x, snake.front().y);
     }
 
     void rollApple() {
         glm::ivec2 pos;
         
-        std::random_device dev;
-        std::mt19937 rng(dev());
-        std::uniform_int_distribution<int> distX(0,WIDTH-1), distY(0, HEIGHT-1); 
-        
         do {
-            pos.x = distX(rng);
-            pos.y = distY(rng);
-        } while(std::find(snake.begin(), snake.end(), pos) != snake.end() || pos == apple);
+            pos.x = distX(rd);
+            pos.y = distY(rd);
+            //std::cout << "Dice: " << pos.x << ' ' << pos.y << std::endl;
+        } while(std::find(snake.begin(), snake.end(), pos) != snake.end());
 
         apple = pos;
     }
 
 private:
+    std::default_random_engine rd{std::random_device()()};
+    std::uniform_int_distribution<int> distX, distY;
     double ts = 0;
     std::deque<glm::ivec2> snake;
     int incX[4] = {0, 1, 0, -1};
     int incY[4] = {-1, 0, 1, 0};
-    int dir = 0;
+    int dir = 0, newDir = 0;
     glm::ivec2 apple;
-    bool dead = false;
+    bool dead;
     float threshold = 0.4;
 
 };
---
+++ README.md
@@ -0,0 +1,5 @@
+# Snake demo Fly Engine
+
+This is a demo game made to prove the capabilities of the Fly Engine in a simple 2D game
+
+To run it, read the requirements of the [vulkan-engine](https://github.com/dangarcar/vulkan-engine) and run run.ps1 on the root folder
